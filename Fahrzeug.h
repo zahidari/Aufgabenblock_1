8cb34aa9bed1798053c2d7ad8409ff76c2b0e285
zahidari <100021451+zahidari@users.noreply.github.com>
2023-11-27 09:33:05 +0100

    Update Fahrzeug.h
    getter setter, constractor

--- Fahrzeug.h
+++ Fahrzeug.h
@@ -9,32 +9,48 @@ using namespace std;
 class Fahrzeug
 {
 private:
-	string const p_sName;
-	int const p_iID;
-	static int p_iMaxID;
-	
+	const string  p_sName; // default "noName"
+	const int  p_iID;
+	const double p_iMaxGeschwindigkeit; // default 100
+
+	static int p_iMaxID;//initalized in fahrzeug.cpp begins with 0
+
+	double p_dGesamtStraecke=0;
+	double p_dGesamtZeit =0; // gesamte zeit des Fahrzeugs
+	double p_dZeit = 0; // die zeit die zuletzt simuliert ist.
+
 
 public:
 	string testString = "public test string.";
 
 	//constructor
-	Fahrzeug(string name = "");// default name is empty
+	Fahrzeug(
+		string name = "noName",
+		double maxGeschwindigkeit = 100
+	);
+
+	// copy constractor gelöscht wegen dynamische elemente
+	Fahrzeug(const Fahrzeug&) = delete; 
+
 	//destructor
 	virtual ~Fahrzeug();
 
-	// getter setter
-	string getName() {
-		return p_sName;
-	}
-	int getID() {
-		return p_iID;
-	}
-
+	// getter
+	string getName() {return p_sName;}
+	int getID() {return p_iID;}
+	double getMaxGeschwindigkeit() {return p_iMaxGeschwindigkeit;}
+	double getGesamtStraeke() {return p_dGesamtStraecke;}
+	double getGesamtZeit() { return p_dGesamtZeit; }
+	double getZeit() { return p_dZeit; }
+	// Setter
+	void setGesamtStraeke(double x) {  p_dGesamtStraecke = x; }
+	void setGesamtZeit(double x) {  p_dGesamtZeit = x; }
+	void setZeit(double x) {  p_dZeit = x; }
 
 	// Methoden
-	void vAusgabe() {
-		cout << "ID:" << getID()
-			<< " Fahrzeug: " << getName()
-			<< endl;
-	}
+	void vAusgabe();
 };
+
+
+
+
