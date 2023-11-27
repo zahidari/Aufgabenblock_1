ccac33dc18629960a7b4c32cd27ab4fdd7d868cf
zahidari <100021451+zahidari@users.noreply.github.com>
2023-11-27 09:32:33 +0100

    Update Fahrzeug.cpp

--- Fahrzeug.cpp
+++ Fahrzeug.cpp
@@ -5,15 +5,36 @@ int Fahrzeug::p_iMaxID = 0;
 
 
 //constractor
-Fahrzeug::Fahrzeug(string name) :p_sName(name), p_iID(++p_iMaxID) {
-	cout <<"ID:"<< p_iID 
-		<< " Fahrzeug: " << p_sName 
-		<<"  wurde erstellt." << endl;
-	
+Fahrzeug::Fahrzeug(
+	string name,
+	double maxGeschwindigkeit
+)
+	:p_sName(name),
+	p_iID(++p_iMaxID),
+	p_iMaxGeschwindigkeit((maxGeschwindigkeit>=0)? maxGeschwindigkeit:0)
+{
+
+	cout << "ID:" << p_iID
+		<< " Fahrzeug: " << p_sName
+		<< "  wurde erstellt." << endl;
+
 }
+
+// destructor
 Fahrzeug::~Fahrzeug(){
 	cout << "ID:" << p_iID
 		<< " Fahrzeug: " << p_sName
 		<< "  wurde GELOESCHT." << endl;
+}
+
+// Methoden
+void Fahrzeug::vAusgabe() {
+
+
+	cout << Fahrzeug::getID()
+		<< Fahrzeug::getName()
+		<< Fahrzeug::getMaxGeschwindigkeit()
+		<< Fahrzeug::getGesamtStraeke()
+		<<endl;
 }
 
