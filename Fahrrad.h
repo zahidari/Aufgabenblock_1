3993ba6ab2b06e036be89a7c579671eb403396f4
zahidari <100021451+zahidari@users.noreply.github.com>
2023-11-30 09:22:21 +0100

    Update Fahrrad.h

--- Fahrrad.h
+++ Fahrrad.h
@@ -1,10 +1,23 @@
 #pragma once
 #include "Fahrzeug.h"
-class Fahrrad : public Fahrzeug
+class Fahrrad :
+    public Fahrzeug
 {
 private:
+protected:
+    
 public:
-	void vAusgabe() const override;
-	void vSimulieren() override;
+    Fahrrad(
+        string name = "noName",
+        double maxGeschwindigkeit = 20
+    );
+    ~Fahrrad();
+    Fahrrad(const Fahrrad&) = delete;
+
+
+    void vAusgabe(ostream&) const override;
+    void vSimulieren() override;
+    double dTanken(double dMenge) override;
+    double dGeschwindigkeit() override;
 };
 
