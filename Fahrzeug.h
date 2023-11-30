62c3208a37d49528599a9b0a5b2861dff4d81eca
zahidari <100021451+zahidari@users.noreply.github.com>
2023-11-30 09:22:52 +0100

    Update Fahrzeug.h

--- Fahrzeug.h
+++ Fahrzeug.h
@@ -10,14 +10,15 @@ extern double dGlobaleZeit;
 class Fahrzeug
 {
 protected:
+	
 	double p_dGesamtStraecke = 0;
 	double p_dGesamtZeit = 0; // gesamte zeit des Fahrzeugs
 	double p_dZeit = 0; // die zeit die zuletzt simuliert ist.
-private:
-	const string  p_sName; // default "noName"
-	const int  p_iID;
-	const double p_dMaxGeschwindigkeit; // default 100
 
+	string  p_sName; // default "noName"
+	const int  p_iID;
+	double p_dMaxGeschwindigkeit ; // default 100
+	double p_dGeschwindigkeit = p_dMaxGeschwindigkeit;
 	static int p_iMaxID;//initalized in fahrzeug.cpp begins with 0
 
 	
@@ -46,17 +47,21 @@ public:
 	double getGesamtZeit() { return p_dGesamtZeit; }
 	double getZeit() { return p_dZeit; }
 	// Setter
+	void setName(string s) { p_sName = s; }
 	void setGesamtStraeke(double x) {  p_dGesamtStraecke = x; }
 	void setGesamtZeit(double x) {  p_dGesamtZeit = x; }
 	void setZeit(double x) {  p_dZeit = x; }
-
+	void setGeschwindigkeit(double x) { p_dGeschwindigkeit = x; }
 	// Methoden
-	virtual void vAusgabe() const;
+	virtual void vAusgabe(ostream&) const;
 	static void vKopf() ; // weil es static ist cosnt geht nicht!
 	virtual void vSimulieren();
-	virtual double dTanken(double menge);
+	virtual double dTanken(double dMenge = std::numeric_limits<double>::infinity());
+	virtual double dGeschwindigkeit(); // Pure virtual method
+	bool operator<(const Fahrzeug& ) const;
+	//Fahrzeug& operator=(const Fahrzeug& );
 };
 
-
+std::ostream& operator<<(std::ostream& , Fahrzeug& );
 
 
