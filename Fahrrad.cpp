959d9892b63e695b5f66c53f928facf11268cb4f
zahidari <100021451+zahidari@users.noreply.github.com>
2023-11-30 09:22:06 +0100

    Update Fahrrad.cpp

--- Fahrrad.cpp
+++ Fahrrad.cpp
@@ -1,11 +1,38 @@
 #include "Fahrrad.h"
+Fahrrad::Fahrrad(string name, double maxGeschwindigkeit)
+    : Fahrzeug(name, maxGeschwindigkeit) {
+    // Additional initialization for a bicycle, if necessary
+}
 
-void Fahrrad::vAusgabe() const
+Fahrrad::~Fahrrad()
 {
-	Fahrzeug::vAusgabe();
+    Fahrzeug::~Fahrzeug();
+}
+
+void Fahrrad::vAusgabe(ostream& o) const
+{
+    Fahrzeug::vAusgabe(o);
 }
 
 void Fahrrad::vSimulieren()
 {
-	Fahrzeug::vSimulieren();
+    Fahrzeug::vSimulieren();
+
+}
+
+double Fahrrad::dTanken(double dMenge) {
+    //Fahrrad::setGeschwindigkeit(20);
+	return 0;
+}
+
+double Fahrrad::dGeschwindigkeit() {
+    // Berechne die aktuelle Geschwindigkeit basierend auf der zurückgelegten Strecke
+    double abnahmeProzent = (getGesamtStraeke() / 20) * 10;
+    double reduzierteGeschwindigkeit = getMaxGeschwindigkeit() * (1 - (abnahmeProzent / 100));
+    if (reduzierteGeschwindigkeit < 12)
+        reduzierteGeschwindigkeit = 12;
+    setGeschwindigkeit(reduzierteGeschwindigkeit);
+
+    //return (reduzierteGeschwindigkeit > 12) ? reduzierteGeschwindigkeit : 12;
+    return reduzierteGeschwindigkeit;
 }
